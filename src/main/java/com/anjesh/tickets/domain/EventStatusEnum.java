5bc6d87278ebd33e960c64668957f2ae1d93fdbc
AnjeshDash <143063198+AnjeshDash@users.noreply.github.com>
2026-01-26 02:55:58 +0530

    Initial commit

---
+++ src/main/java/com/anjesh/tickets/domain/EventStatusEnum.java
@@ -0,0 +1,5 @@
+package com.anjesh.tickets.domain;
+
+public enum EventStatusEnum {
+    DRAFT, PUBLISHED, CANCELLED, COMPLETED
+}
---
+++ src/test/java/com/anjesh/tickets/TicketsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anjesh.tickets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
