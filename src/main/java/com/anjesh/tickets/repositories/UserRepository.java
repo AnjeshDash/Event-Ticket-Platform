cd21d6960d6e750bbdd9f962cf164230a138fc4f
AnjeshDash <143063198+AnjeshDash@users.noreply.github.com>
2026-01-26 09:21:26 +0530

    Created user repo and implemented User provisioning filter

---
+++ src/main/java/com/anjesh/tickets/repositories/UserRepository.java
@@ -0,0 +1,11 @@
+package com.anjesh.tickets.repositories;
+
+import com.anjesh.tickets.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, UUID> {
+}
