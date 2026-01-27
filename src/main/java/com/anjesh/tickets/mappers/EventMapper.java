5ddae4f71f4cadf6bdaaf4e382bbf9b1bd685c0c
AnjeshDash <143063198+AnjeshDash@users.noreply.github.com>
2026-01-28 04:35:56 +0530

    Created Dtos required for event updating

--- src/main/java/com/anjesh/tickets/mappers/EventMapper.java
+++ src/main/java/com/anjesh/tickets/mappers/EventMapper.java
@@ -2,6 +2,8 @@ package com.anjesh.tickets.mappers;
 
 import com.anjesh.tickets.domain.CreateEventRequest;
 import com.anjesh.tickets.domain.CreateTicketTypeRequest;
+import com.anjesh.tickets.domain.UpdateEventRequest;
+import com.anjesh.tickets.domain.UpdateTicketTypeRequest;
 import com.anjesh.tickets.domain.dtos.*;
 import com.anjesh.tickets.domain.entities.Event;
 import com.anjesh.tickets.domain.entities.TicketType;
@@ -24,5 +26,13 @@ public interface EventMapper {
     GetEventDetailsTicketTypesResponseDto toGetEventTicketTypesResponseDto(TicketType ticketType);
 
     GetEventDetailsResponseDto toGetEventDetailsResponseDto(Event event);
+
+    UpdateTicketTypeRequest fromDto(UpdateTicketTypeRequestDto dto);
+
+    UpdateEventRequest fromDto(UpdateEventRequestDto dto);
+
+    UpdateTicketTypeResponseDto toUpdateTicketTypeResponseDto(TicketType ticketType);
+
+    UpdateEventResponseDto toUpdateEventResponseDto(Event event);
 
 }
