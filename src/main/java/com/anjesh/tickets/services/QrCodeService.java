ef64c9a0f6e7139a0c37936be56f2a1a356baf6d
AnjeshDash <143063198+AnjeshDash@users.noreply.github.com>
2026-01-29 03:58:57 +0530

    Implemented the get QR Code endpoint

--- src/main/java/com/anjesh/tickets/services/QrCodeService.java
+++ src/main/java/com/anjesh/tickets/services/QrCodeService.java
@@ -3,7 +3,11 @@ package com.anjesh.tickets.services;
 import com.anjesh.tickets.domain.entities.QrCode;
 import com.anjesh.tickets.domain.entities.Ticket;
 
+import java.util.UUID;
+
 public interface QrCodeService {
 
     QrCode generateQrCode(Ticket ticket);
+
+    byte[] getQrCodeImageForUserAndTicket(UUID userId, UUID ticketId);
 }
--- src/main/java/com/anjesh/tickets/services/impl/QrCodeServiceImpl.java
+++ src/main/java/com/anjesh/tickets/services/impl/QrCodeServiceImpl.java
@@ -4,6 +4,7 @@ import com.anjesh.tickets.domain.entities.QrCode;
 import com.anjesh.tickets.domain.entities.QrCodeStatusEnum;
 import com.anjesh.tickets.domain.entities.Ticket;
 import com.anjesh.tickets.exceptions.QrCodeGenerationException;
+import com.anjesh.tickets.exceptions.QrCodeNotFoundException;
 import com.anjesh.tickets.repositories.QrCodeRepository;
 import com.anjesh.tickets.services.QrCodeService;
 import com.google.zxing.BarcodeFormat;
@@ -12,6 +13,7 @@ import com.google.zxing.client.j2se.MatrixToImageWriter;
 import com.google.zxing.common.BitMatrix;
 import com.google.zxing.qrcode.QRCodeWriter;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import javax.imageio.ImageIO;
@@ -23,6 +25,7 @@ import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class QrCodeServiceImpl implements QrCodeService {
 
     private static final int QR_HEIGHT = 300;
@@ -50,6 +53,20 @@ public class QrCodeServiceImpl implements QrCodeService {
 
         }
     }
+
+    @Override
+    public byte[] getQrCodeImageForUserAndTicket(UUID userId, UUID ticketId) {
+        QrCode qrCode = qrCodeRepository.findByTicketIdAndTicketPurchaserId(ticketId, userId)
+                .orElseThrow(QrCodeNotFoundException::new);
+
+        try {
+            return Base64.getDecoder().decode(qrCode.getValue());
+        }catch (IllegalArgumentException ex){
+            log.error("Invalid base64 QR Code for ticket ID: {}",ticketId, ex);
+            throw new QrCodeNotFoundException();
+        }
+    }
+
     private String generateQrCodeImage(UUID uniqueId) throws WriterException, IOException {
         BitMatrix bitMatrix = qrCodeWriter.encode(
                 uniqueId.toString(),
